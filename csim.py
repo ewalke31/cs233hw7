77ac352efe9c23b2e217472af9c0a1b13af74234
Eric Walker <ewalke31@jhu.edu>
2016-04-12 17:01:42 -0400

    added all necessary methods

--- csim.py
+++ csim.py
@@ -15,10 +15,21 @@ def main():
         sys.exit(1)
     if (int(sys.argv[4]) != 0 and int(sys.argv[4]) != 1) or \
        (int(sys.argv[5]) != 0 and int(sys.argv[5]) != 1) or \
-       (int(sys.argv[6]) != 0 and int(sys.argv[6]) != 1):
+       (int(sys.argv[6]) != 0 and int(sys.argv[6]) != 1) or \
+       (int(sys.argv[4]) == 0 and int(sys.argv[5]) == 0):
         print("Invalid cache design parameters.")
         sys.exit(1)
     inputList = readInput(sys.argv[7])
+    outputInfo = simulate(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4],
+                          sys.argv[5], sys.argv[6], inputList)
+    output(outputInfo)
+    return
+
+
+# simulates the cache
+def simulate(numSets, numBlocks, numBytes, walloc, wtorb, evictMethod, trace):
+    outputInfo = [0, 0, 0, 0, 0, 0, 0]
+    return outputInfo
 
 
 # checks if a number is a power of two and greater than zero
@@ -38,6 +49,17 @@ def readInput(trace):
         inputList.append(line)
     f.close()
     return inputList
+
+
+# prints out the required output information
+def output(outputInfo):
+    print("Total loads: " + str(outputInfo[0]))
+    print("Total stores: " + str(outputInfo[1]))
+    print("Load hits: " + str(outputInfo[2]))
+    print("Load misses: " + str(outputInfo[3]))
+    print("Store hits: " + str(outputInfo[4]))
+    print("Store misses: " + str(outputInfo[5]))
+    print("Total cycles: " + str(outputInfo[6]))
 
 
 if __name__ == "__main__":
